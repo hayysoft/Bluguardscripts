21250d6415478085a1596c72364f1fd00bbb74db
hayysoft <info@hayysoft.com>
2021-06-09 14:05:53 +0800

    Make changes - Wed Jun  9 14:05:53 2021

--- Main_Program.py
+++ Main_Program.py
@@ -10,8 +10,8 @@ from Broker_GetIoTData import Message
 
 
 client_instance = Client()  # Instantiate mqtt client
-client_instance.client_.on_message = Message  # Set on_message function to receive 
-									 # data from gateway
+client_instance.client_.on_message = Message  # Set on_message function to receive
+									          # data from gateway
 client_instance.Start_connection()  # Initiate connection
 
 
