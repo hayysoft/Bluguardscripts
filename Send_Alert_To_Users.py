9960209d5de05dcbf3b2f85799744f7defdfea3b
Hayysoft <info@hayysoft.com>
2021-10-11 08:41:14 +0000

    New changes

--- Send_Alert_To_Users.py
+++ Send_Alert_To_Users.py
@@ -45,7 +45,7 @@ def Send_Notification():
 	          TIMESTAMPDIFF(SECOND, Alert_Datetime, CURRENT_TIMESTAMP()) < %s;    
 	'''
 
-	parameters = (30, 80)
+	parameters = (30, 70)
 	Cursor.execute(query, parameters)
 	results = dictfetchall(Cursor)
 	for row in results:
@@ -125,57 +125,61 @@ def Testing():
 	query = 'SELECT * FROM TBL_Alert'
 	Cursor.execute(query)
 	results = dictfetchall(Cursor)
-	for row in results:
-		Alert_Code = row['Alert_Code']
-		Alert_Datetime = row['Alert_Datetime']
-		Device_ID = row['Device_ID']
-
-		query = '''
-			SELECT * FROM TBL_User
-				WHERE User_ID IN (
-					SELECT User_ID FROM TBL_Wearer
-						WHERE Wearer_ID IN (
-							SELECT Wearer_ID FROM TBL_Device
-								WHERE Device_ID = %s
-						)
-				)
-		'''
-		parameter = (Device_ID,)
-		Cursor.execute(query, parameter)
-		results_ = dictfetchall(Cursor)
-		for row_ in results_:
-			Phone_Nr = row_['Telephone_Number']
-			User_Name = row_['User_Name']
-			User_ID = row_['User_ID']
+	if results == []:
+		print('Not Notification!')
+		time.sleep(2)
+	else:
+		for row in results:
+			Alert_Code = row['Alert_Code']
+			Alert_Datetime = row['Alert_Datetime']
+			Device_ID = row['Device_ID']
 
 			query = '''
-				SELECT * FROM TBL_Wearer
-					WHERE User_ID = %s
+				SELECT * FROM TBL_User
+					WHERE User_ID IN (
+						SELECT User_ID FROM TBL_Wearer
+							WHERE Wearer_ID IN (
+								SELECT Wearer_ID FROM TBL_Device
+									WHERE Device_ID = %s
+							)
+					)
 			'''
-			parameter = (User_ID,)
+			parameter = (Device_ID,)
 			Cursor.execute(query, parameter)
-			results_tbl_wearer = dictfetchall(Cursor)
+			results_ = dictfetchall(Cursor)
+			for row_ in results_:
+				Phone_Nr = row_['Telephone_Number']
+				User_Name = row_['User_Name']
+				User_ID = row_['User_ID']
 
-			for row3 in results_tbl_wearer:
-				Wearer_Nick = row3['Wearer_Nick']
-				Wearer_ID = row3['Wearer_ID']
+				query = '''
+					SELECT * FROM TBL_Wearer
+						WHERE User_ID = %s
+				'''
+				parameter = (User_ID,)
+				Cursor.execute(query, parameter)
+				results_tbl_wearer = dictfetchall(Cursor)
 
-				Alert_Type = Get_Alert_Type(int(Alert_Code))
+				for row3 in results_tbl_wearer:
+					Wearer_Nick = row3['Wearer_Nick']
+					Wearer_ID = row3['Wearer_ID']
 
-			print(json.dumps({
-				'Alert_Code': Alert_Code,
-				'Alert_Type': Alert_Type,
-				'Alert_Datetime': Alert_Datetime,
-				'Telephone_Number': Phone_Nr,
-				'User_ID': User_ID,
-				'User_Name': User_Name,
-				'Wearer_Nick': Wearer_Nick,
-				'Wearer_ID': Wearer_ID
-			}, indent=4, default=default))
-			# f'Hi there, please check Wearer "{{1}}" with Wearer ID "{{2}}" is having "{{3}}", captured at {{4}}.'
-			# message_template = f'Hi there, please check User "{User_Name}" is having "{Alert_Type}", captured at {Alert_Datetime}'
-			message_template = f'Hi there, please check Wearer Nickname "{Wearer_Nick}" with Wearer ID "{Wearer_ID}", is having "{Alert_Type}", captured at {Alert_Datetime}.'
-			print(message_template)
+					Alert_Type = Get_Alert_Type(int(Alert_Code))
+
+				print(json.dumps({
+					'Alert_Code': Alert_Code,
+					'Alert_Type': Alert_Type,
+					'Alert_Datetime': Alert_Datetime,
+					'Telephone_Number': Phone_Nr,
+					'User_ID': User_ID,
+					'User_Name': User_Name,
+					'Wearer_Nick': Wearer_Nick,
+					'Wearer_ID': Wearer_ID
+				}, indent=4, default=default))
+				# f'Hi there, please check Wearer "{{1}}" with Wearer ID "{{2}}" is having "{{3}}", captured at {{4}}.'
+				# message_template = f'Hi there, please check User "{User_Name}" is having "{Alert_Type}", captured at {Alert_Datetime}'
+				message_template = f'Hi there, please check Wearer Nickname "{Wearer_Nick}" with Wearer ID "{Wearer_ID}", is having "{Alert_Type}", captured at {Alert_Datetime}.'
+				print(message_template)
 
 
 # Testing()
